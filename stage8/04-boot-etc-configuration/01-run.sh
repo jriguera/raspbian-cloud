e22bb0918f1ac6922664727bbe1c150c1d7af469
Jose Riguera <jriguera@gmail.com>
2018-10-25 00:24:04 +0200

    Improve boot-etc config management

--- stage8/04-boot-etc-configuration/01-run.sh
+++ stage8/04-boot-etc-configuration/01-run.sh
@@ -1,10 +1,23 @@
 #!/bin/bash -e
 
+BOOT_CONFIG_FOLDER=/boot/etc
+
+# remove trailing slash
+BOOT_CONFIG_FOLDER=${BOOT_CONFIG_FOLDER%%+(/)}
+# start slash
+BOOT_CONFIG_FOLDER=${BOOT_CONFIG_FOLDER##+(/)}
+
 # script
 install -m 755 -g root -o root files/bin/cp-files-permissions.sh ${ROOTFS_DIR}/usr/local/bin
 
 # systemd units
 install -m 644 -g root -o root files/systemd/cp-files-permissions@.service ${ROOTFS_DIR}/etc/systemd/system
-# /boot folder services
-install -m 644 -g root -o root files/systemd/cp-files-permissions@.service ${ROOTFS_DIR}/etc/systemd/system/cp-files-permissions@boot-etc.service
+
+on_chroot <<EOF
+CONFIGDIR=$(systemd-escape --path ${BOOT_CONFIG_FOLDER})
+ln -sfr /etc/systemd/system/cp-files-permissions@.service /etc/systemd/system/cp-files-permissions@${CONFIGDIR}.service
+mkdir -p /${BOOT_CONFIG_FOLDER}
+# Enable service
+systemctl enable cp-files-permissions@${CONFIGDIR}.service
+EOF
 
