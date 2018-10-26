ded81692482be31b3803505a6472c1c412581266
Jose Riguera <jriguera@gmail.com>
2018-10-27 00:53:18 +0200

    Enable hostapd by default

--- stage8/40-hostapd/01-run.sh
+++ stage8/40-hostapd/01-run.sh
@@ -5,11 +5,10 @@ DEVICE=wlan0
 install -m 644 -g root -o root files/default ${ROOTFS_DIR}/etc/default/hostapd
 
 # Systemd service
-install -m 644 -g root -o root files/systemd/hostapd@.service ${ROOTFS_DIR}/etc/systemd/system
+install -m 644 -g root -o root files/systemd/hostapd@.service ${ROOTFS_DIR}/lib/systemd/system
 
 on_chroot <<EOF
-ln -sfr /etc/systemd/system/hostapd@.service /etc/systemd/system/hostapd@${DEVICE}.service
-# Enable services
+# Enable service
 systemctl enable hostapd@${DEVICE}.service
 EOF
 
