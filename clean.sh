8d2d00de43dd73e6ef1b08a2175a1e1b43748a78
Jose Riguera <jriguera@gmail.com>
2018-10-27 11:22:32 +0200

    improve clean

--- clean.sh
+++ clean.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-umount work/*-RaspbianCloud/stage0/rootfs/sys
-umount work/*-RaspbianCloud/stage0/rootfs/dev/pts
-umount work/*-RaspbianCloud/stage0/rootfs/dev
-umount work/*-RaspbianCloud/stage0/rootfs/proc
+umount work/*-RaspbianCloud/stage*/rootfs/sys
+umount work/*-RaspbianCloud/stage*/rootfs/dev/pts
+umount work/*-RaspbianCloud/stage*/rootfs/dev
+umount work/*-RaspbianCloud/stage*/rootfs/proc
 
 rm -rf work/*-RaspbianCloud
 
