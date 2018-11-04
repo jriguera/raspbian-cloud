916f05561f9d3014f49990befd7ef9fda971a487
Jose Riguera <jriguera@gmail.com>
2018-11-04 15:20:51 +0100

    New submodule for rpi-btrfs

---
+++ stage8/51-btrfs-volume-data/00-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# Copy binary
+install -m 755 -g root -o root rpi-btrfs/bin/* ${ROOTFS_DIR}/bin
+install -m 755 -g root -o root rpi-btrfs/requirements.txt ${ROOTFS_DIR}/tmp
+
+## Install docker-compose
+on_chroot <<EOF
+pip3 install -r /tmp/requirements.txt
+EOF
+
