0cdb8ebf1bbfda3c529f911f0995e16531d67177
Jose Riguera <jriguera@gmail.com>
2019-04-14 22:47:13 +0200

    force ln

--- stage8/30-opensmtpd/01-run.sh
+++ stage8/30-opensmtpd/01-run.sh
@@ -7,7 +7,7 @@ install -m 644 -g root -o root files/default ${ROOTFS_DIR}/etc/default/smtp
 # Create config folder
 on_chroot << EOF
 mkdir -p /etc/mail
-ln -s ../aliases /etc/mail/aliases
+ln -sf ../aliases /etc/mail/aliases
 touch /etc/mail/smtpd.conf.local
 chown -R root:opensmtpd /etc/mail
 EOF
