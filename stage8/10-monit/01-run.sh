06bb413584ba53c1d88c85a12f39599a04d82823
Jose Riguera <jriguera@gmail.com>
2019-04-14 17:38:26 +0200

    added monit templates

--- stage8/10-monit/01-run.sh
+++ stage8/10-monit/01-run.sh
@@ -7,6 +7,7 @@ mkdir -p ${ROOTFS_DIR}/etc/monit/conf.d
 install -m 644 -g root -o root files/conf.d/* ${ROOTFS_DIR}/etc/monit/conf.d/
 rm -f ${ROOTFS_DIR}/etc/monit/conf-available/*
 install -m 644 -g root -o root files/conf-available/* ${ROOTFS_DIR}/etc/monit/conf-available/
+install -m 644 -g root -o root files/templates/* ${ROOTFS_DIR}/etc/monit/templates/
 
 # Systemd service
 install -m 644 -g root -o root files/systemd/monit.service ${ROOTFS_DIR}/lib/systemd/system
