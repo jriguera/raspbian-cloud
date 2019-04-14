eb6f399864de26cf4adbb1a76baf9537c6225ff2
Jose Riguera <jriguera@gmail.com>
2019-04-14 15:59:53 +0200

    improve avahi

--- stage8/12-avahi/01-run.sh
+++ stage8/12-avahi/01-run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
 # Config
+install -m 644 -g root -o root files/default ${ROOTFS_DIR}/etc/default/avahi-daemon
 install -m 644 -g root -o root files/avahi-daemon.conf ${ROOTFS_DIR}/etc/avahi/
+
+
+# systemctl override
+install -m 775 -g root -o root -d ${ROOTFS_DIR}/etc/systemd/system/avahi-daemon.service.d
+install -m 644 -g root -o root files/systemd/override.conf ${ROOTFS_DIR}/etc/systemd/system/avahi-daemon.service.d
 
 # Enable ssh
 on_chroot << EOF
@@ -16,4 +22,3 @@ mkdir -p /etc/monit/conf-enabled
 cd /etc/monit/conf-enabled
 ln -sf ../conf-available/avahi-daemon
 EOF
-
