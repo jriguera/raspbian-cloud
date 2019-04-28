f4efec085ec2c0cd9b6174e61c753a392c4f4bde
Jose Riguera <jriguera@gmail.com>
2019-04-28 14:21:50 +0200

    fix journal deletion

--- stage9/95-delete-journal/00-run.sh
+++ stage9/95-delete-journal/00-run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
 # Delete journal
-on_chroot << EOF
-journalctl --rotate
-journalctl --vacuum-time=1s
-EOF
+#on_chroot << EOF
+#journalctl --rotate
+#journalctl --vacuum-time=1s
+#EOF
 
+rm -rf ${ROOTFS_DIR}/var/log/journal/*
