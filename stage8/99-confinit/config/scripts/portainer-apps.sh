6a1dda24ef30eb162e21d5a5a8108d1309889ad3
Jose Riguera <jriguera@gmail.com>
2019-05-27 18:37:13 +0200

    move portainer templates to etc

--- stage8/99-confinit/config/scripts/portainer-apps.sh
+++ stage8/99-confinit/config/scripts/portainer-apps.sh
@@ -7,7 +7,7 @@
 mkdir -p /data/portainer
 nohup curl --retry-max-time 600 --max-time 30 --retry 10 --retry-delay 0 \
      -L {{ .Data.docker.portainer.apps }} \
-     -o /data/portainer/apps.json \
+     -o /etc/portainer/apps.json \
 </dev/null >/tmp/run/$(basename -- "$0").log  2>&1 &
 {{ end }}
 {{ end }}
