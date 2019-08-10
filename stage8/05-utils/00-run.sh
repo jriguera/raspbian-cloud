050ee18c863320a398a31f44cab32c8403308c6c
Jose Riguera <jriguera@gmail.com>
2019-08-10 16:41:14 +0200

    install lazydocker and gotop

---
+++ stage8/05-utils/00-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+VERSION="0.6.4"
+ARCH="armv6"
+
+# Install and enable Prometheus node_exporter
+on_chroot <<EOF
+curl -sSL  https://github.com/jesseduffield/lazydocker/releases/download/v${VERSION}/lazydocker_${VERSION}_Linux_${ARCH}.tar.gz -o /tmp/lazydocker.tar.gz
+tar -xvf /tmp/lazydocker.tar.gz -C /usr/sbin/ --wildcards --no-anchored lazydocker
+chown root.root /usr/sbin/lazydocker
+chmod a+x /usr/sbin/lazydocker
+rm -f /tmp/lazydocker.tar.gz
+EOF
+
---
+++ stage8/05-utils/01-run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+VERSION="3.0.0"
+ARCH="arm6"
+
+# Install and enable Prometheus node_exporter
+on_chroot <<EOF
+curl -sSL https://github.com/cjbassi/gotop/releases/download/${VERSION}/gotop_${VERSION}_linux_${ARCH}.tgz -o /tmp/gotop.tar.gz
+tar -xvf /tmp/gotop.tar.gz -C /usr/sbin/ --wildcards --no-anchored gotop
+chown root.root /usr/sbin/gotop
+chmod a+x /usr/sbin/gotop
+rm -f /tmp/gotop.tar.gz
+EOF
+
