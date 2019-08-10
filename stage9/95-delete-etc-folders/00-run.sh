fc51354c630ff27d9bca7e699be854473c129dbb
Jose Riguera <jriguera@gmail.com>
2019-08-10 16:44:35 +0200

    delete extra folders from etc

--- stage9/95-delete-etc-folders/00-run.sh
+++ stage9/95-delete-etc-folders/00-run.sh
@@ -5,3 +5,9 @@ rm -rf ${ROOTFS_DIR}/etc/NetworkManager
 rm -rf ${ROOTFS_DIR}/etc/X11
 rm -rf ${ROOTFS_DIR}/etc/ppp
 rm -rf ${ROOTFS_DIR}/etc/anacrontab
+
+rm -rf ${ROOTFS_DIR}/etc/binfmt.d
+rm -rf ${ROOTFS_DIR}/etc/apache2
+rm -rf ${ROOTFS_DIR}/etc/gdb
+rm -rf ${ROOTFS_DIR}/etc/lighttpd
+rm -rf ${ROOTFS_DIR}/etc/python2.7
