cc81f3ff9055667a2aae512765198fb9c4a21415
Jose Riguera <jriguera@gmail.com>
2019-08-10 16:45:49 +0200

    remove extra packages not needed

--- stage9/01-remove-packages-set-defaults/00-run.sh
+++ stage9/01-remove-packages-set-defaults/00-run.sh
@@ -2,16 +2,18 @@
 
 on_chroot << EOF
 apt-get -y purge \
-  xauth \
-  samba-common \
-  cifs-utils \
-  libnfsidmap2 \
-  nfs-common \
-  xdg-user-dirs \
+  python python-minimal python2 python2-minimal python2.7 python2.7-minimal libpython2.7-minimal \
+  libfreetype6-dev libmnl-dev libraspberrypi-dev \
+  apt-transport-https \
+  xauth xdg-user-dirs \
+  samba-common cifs-utils \
+  libnfsidmap2 nfs-common \
   triggerhappy \
   paxctld \
   btrbk \
-  pv 
+  pv \
+  javascript-common \
+  gdb
 EOF
 
 rm -rf "${ROOTFS_DIR}/etc/cifs-utils"
@@ -22,6 +24,8 @@ rm -rf "${ROOTFS_DIR}/etc/samba"
 rm -f "${ROOTFS_DIR}/etc/pam.d/samba"
 rm -f "${ROOTFS_DIR}/etc/dhcp/dhclient-enter-hooks.d/samba"
 rm -f "${ROOTFS_DIR}/etc/init.d/resize2fs_once"
+
+rm -rf "${ROOTFS_DIR}/etc/ifplugd"
 
 # Attention: "  quietquiet" is on purpose in order to hack the greq in /usr/lib/raspi-config/init_resize.sh
 # and keep quiet in the kernel cmd
