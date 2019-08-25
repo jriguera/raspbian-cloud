71910219947f584b100f71fd908c194149c212e2
Jose Riguera <jriguera@gmail.com>
2019-08-26 00:23:21 +0200

    remove /etc/dhcp/dhclient-exit-hooks.d/timesyncd

--- stage9/01-systemd-settings/01-run.sh
+++ stage9/01-systemd-settings/01-run.sh
@@ -10,6 +10,7 @@ install -m 644 -g root -o root files/journald.conf ${ROOTFS_DIR}/etc/systemd/
 on_chroot << EOF
 # timedatectl set-ntp true
 systemctl enable systemd-timesyncd
+rm -f /etc/dhcp/dhclient-exit-hooks.d/timesyncd
 EOF
 
 # Add watchdog
