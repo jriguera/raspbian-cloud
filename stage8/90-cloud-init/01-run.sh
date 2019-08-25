b03012846a12e9df3f38b803a7e354a2947526e2
Jose Riguera <jriguera@gmail.com>
2019-08-25 23:55:30 +0200

    remove cloud-config dhcp for azure

--- stage8/90-cloud-init/01-run.sh
+++ stage8/90-cloud-init/01-run.sh
@@ -11,6 +11,11 @@ install -m 755 -g root -o root files/scripts/vendor/00-resize-root-fs.sh ${ROOTF
 
 install -m 644 -g root -o root files/bugfix/cc_disk_setup.py ${ROOTFS_DIR}/usr/lib/python3/dist-packages/cloudinit/config/
 
+on_chroot <<EOF
+# This is not azure!
+rm -f /etc/dhcp/dhclient-exit-hooks.d/hook-dhclient
+EOF
+
 # Redirect logs to tty in systemd
 #systemd_tty() {
 #    local tty=$1
