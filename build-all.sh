8af18a8352f32873c3fd04145752381e24d6e671
Jose Riguera <jriguera@gmail.com>
2019-11-09 01:20:04 +0100

    adapt scripts: removed references to mender-convert

--- build-all.sh
+++ build-all.sh
@@ -7,8 +7,5 @@ git submodule update
 echo "Building traditional RPI image ..."
 ./raspbian-cloud-build.sh
 
-echo "Building Mender OTA image ..."
-./mender-ota-build.sh
-
 echo "All done! Have a look in deploy folder:"
 ls -l deploy
