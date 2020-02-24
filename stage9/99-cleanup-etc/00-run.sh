17d366ff68491738e80482cc0d4ad6eb37d7f8d2
Jose Riguera <jriguera@gmail.com>
2020-02-25 00:24:42 +0100

    Delete unused /etc folders

--- stage9/99-cleanup-etc/00-run.sh
+++ stage9/99-cleanup-etc/00-run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
 rm -rf ${ROOTFS_DIR}/etc/staff-group-for-usr-local
-rm -rf ${ROOTFS_DIR}/etc/NetworkManager
 rm -rf ${ROOTFS_DIR}/etc/X11
-rm -rf ${ROOTFS_DIR}/etc/ppp
 rm -rf ${ROOTFS_DIR}/etc/anacrontab
 
 rm -rf ${ROOTFS_DIR}/etc/binfmt.d
