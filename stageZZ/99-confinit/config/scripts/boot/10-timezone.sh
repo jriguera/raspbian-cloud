59ee5543b37a3acdf64407c730e7a59d0ce7d56c
Jose Riguera <jriguera@gmail.com>
2020-03-27 00:00:43 +0100

    update confinit templates to go for full systemd support

--- stageZZ/99-confinit/config/scripts/boot/10-timezone.sh
+++ stageZZ/99-confinit/config/scripts/boot/10-timezone.sh
@@ -6,4 +6,6 @@ echo "{{ .Data.system.timezone }}" > /etc/timezone
 ln -sf /usr/share/zoneinfo/{{ .Data.system.timezone }} /etc/localtime
 echo "done"
 {{ end }}
+
 exit 0
+
--- stageZZ/99-confinit/config/scripts/boot/91-docker-compose.sh
+++ stageZZ/99-confinit/config/scripts/boot/91-docker-compose.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-# Hack to capitalize env vars
-sed -ie 's/^\(.*\)=\(.*\)$/\U\1\E=\2/g' /etc/docker-compose/.env
-
 # Docker
-{{ if .Data.docker.compose }}
+{{ if .Data.docker.Compose }}
 echo -n "* Enable Docker Compose ... "
 rm -f /etc/docker-compose/docker-compose-disabled
-echo "done"
 {{ else }}
 echo -n "* Disable Docker Compose ... "
 echo "Disabled by confinit, $(date)" > /etc/docker-compose/docker-compose-disabled
-echo "done"
 {{ end }}
+
+echo "done"
+
