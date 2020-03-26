083242e0a15e8b2a43f675e718871fe49db30110
Jose Riguera <jriguera@gmail.com>
2020-03-26 22:12:28 +0100

    disable all services from raspberrypi-sys-mods packages

--- stage9/20-disable-raspberrypi-sys-mods/00-run.sh
+++ stage9/20-disable-raspberrypi-sys-mods/00-run.sh
@@ -1,15 +1,23 @@
 #!/bin/bash -e
 
-# This functionality is done by cloud-init or automatically enabled
+# https://www.enricozini.org/blog/2019/himblick/cleanup-raspbian/
 
 on_chroot << EOF
 systemctl disable regenerate_ssh_host_keys.service
+systemctl mask regenerate_ssh_host_keys.service
+
 systemctl disable apply_noobs_os_config.service
-systemctl disable rpi-display-backlight.service
+systemctl mask apply_noobs_os_config.service
+
 systemctl disable sshswitch.service
+systemctl mask sshswitch.service
+
 systemctl disable wifi-country.service
+systemctl mask wifi-country.service
+
 apt-mark hold raspberrypi-sys-mods
 EOF
 
 rm -f "${ROOTFS_DIR}/etc/profile.d/wifi-country.sh"
 rm -f "${ROOTFS_DIR}/etc/sudoers.d/010_pi-nopasswd"
+
