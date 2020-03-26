4a05029495835a5f8e0cb0c2e649fc34d07ad032
Jose Riguera <jriguera@gmail.com>
2020-03-26 21:58:44 +0100

    add ssh-keygen service together with ssh

--- stage8/15-openssh/01-run.sh
+++ stage8/15-openssh/01-run.sh
@@ -6,6 +6,8 @@ install -m 644 -g root -o root files/default ${ROOTFS_DIR}/etc/default/ssh
 # systemctl override
 install -m 775 -g root -o root -d ${ROOTFS_DIR}/etc/systemd/system/ssh.service.d
 install -m 644 -g root -o root files/systemd/override.conf ${ROOTFS_DIR}/etc/systemd/system/ssh.service.d
+# Keygen service
+install -m 644 -g root -o root files/systemd/ssh-keygen.service ${ROOTFS_DIR}/lib/systemd/system
 
 # Enable ssh
 on_chroot << EOF
@@ -15,7 +17,10 @@ sed -i -e 's/^#TCPKeepAlive.*/TCPKeepAlive yes/' /etc/ssh/sshd_config
 sed -i -e 's/^#Compression.*/Compression delayed/' /etc/ssh/sshd_config
 sed -i -e 's/^#MaxStartups.*/MaxStartups 5:50:10/' /etc/ssh/sshd_config
 systemctl enable ssh
+systemctl enable ssh-keygen
 EOF
+
+rm -f "${ROOTFS_DIR}/etc/ssh/"ssh_host_*_key*
 
 # Publish ssh in avahi
 cp ${ROOTFS_DIR}/usr/share/doc/avahi-daemon/examples/ssh.service  ${ROOTFS_DIR}/etc/avahi/services/
