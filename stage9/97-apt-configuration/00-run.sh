9913353276dadc9c661de68f04a8d4d96e35b810
Jose Riguera <jriguera@gmail.com>
2020-03-26 22:59:39 +0100

    move apt configuration

--- stage9/97-apt-configuration/00-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-on_chroot << EOF
-# Disable daily apt unattended updates.
-echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
-# Enable retries, which should reduce the number box buld failures resulting from a temporal network problems.
-echo 'APT::Acquire::Retries "3";' > /etc/apt/apt.conf.d/80retries
-
-EOF
