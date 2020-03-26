d28e1528a9e3bde78e2c69ae4a05704785df05b6
Jose Riguera <jriguera@gmail.com>
2020-03-26 22:03:23 +0100

    Enable nftables service

--- stage8/50-nftables/01-run.sh
+++ stage8/50-nftables/01-run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -e
 
+mkdir -p ${ROOTFS_DIR}/etc/systemd/system/nftables.service.d
+install -m 644 -g root -o root files/systemd/nftables.conf ${ROOTFS_DIR}/etc/systemd/system/nftables.service.d
+
+mkdir -p ${ROOTFS_DIR}/etc/nftables
+
 on_chroot <<EOF
-# Disable service
-systemctl disable nftables.service
+# Enable service
+systemctl enable nftables.service
 EOF
