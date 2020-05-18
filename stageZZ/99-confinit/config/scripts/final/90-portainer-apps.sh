000cdf7830650ae130b383efbcfded857a8cff3f
Jose Riguera <jriguera@gmail.com>
2020-05-18 20:27:03 +0200

    change fields

--- stageZZ/99-confinit/config/scripts/final/90-portainer-apps.sh
+++ stageZZ/99-confinit/config/scripts/final/90-portainer-apps.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 {{ if .Data.docker }}
-{{ if .Data.docker.portainer }}
-{{ if .Data.docker.portainer.apps }}
+{{ if .Data.docker.Portainer }}
+{{ if .Data.docker.Portainer.apps }}
 # see /etc/docker-compose/docker-compose.yml
 mkdir -p /data/portainer
-nohup curl --silent --retry-max-time 600 --max-time 30 --retry 10 --retry-delay 0 --location {{ .Data.docker.portainer.apps }} -o /etc/portainer/apps.json &
+nohup curl --silent --retry-max-time 600 --max-time 30 --retry 10 --retry-delay 0 --location {{ .Data.docker.Portainer.apps }} -o /etc/portainer/apps.json &
 {{ end }}
 {{ end }}
 {{ end }}
